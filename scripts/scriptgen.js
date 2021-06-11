24244b2b169c94e8346c70b95bf7b5fd6e518c64
Zhi Charles Teoh <zc970520@hotmail.com>
2021-06-11 20:55:39 +1000

    Final commit

--- scripts/scriptgen.js
+++ scripts/scriptgen.js
@@ -11,3 +11,15 @@ window.addEventListener('click', function(e) {
         document.getElementById('hamBlock').className = "h__ham-block"; //the same code you've used to hide the menu
     } 
 })
+
+function disableShipping() {
+    var form = document.getElementById("form__shipping");
+    var elements = form.elements;
+    for (var i = 0, len = elements.length; i < len; ++i) {
+        if (elements[i].disabled === true) {
+            elements[i].disabled = false;
+        } else {
+            elements[i].disabled = true;
+        }
+    }
+}
